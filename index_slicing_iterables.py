595b7c627f86e1c7eb6177dc15400a601a4d54d5
CJ2e <cj2e99@gmail.com>
2023-07-08 14:21:11 +0100

    index and slicing iterables

---
+++ index_slicing_iterables.py
@@ -0,0 +1,4 @@
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(lst[0:3])  # prints the values in positions 0,1,2 which are [1, 2, 3]
+print(lst[-2::])  # prints the values in positions -2,-1 which are [8, 9]
